a407ede26ce15a2703883fbc94ce8d82ce6013f4
unknown <sid69740@gmail.com>
2025-08-09 14:49:27 +0530

    Repeating GCD

---
+++ Find Factorial.py
@@ -0,0 +1,17 @@
+def fact(x:int):
+    result=1
+    for x in range(1,x+1):
+        result=x*result
+    return result
+print(fact(5))
+
+def factor(x:int):
+    if x==1 or x==0:
+        return True
+    fact=1
+    y=1
+    while fact<x:
+        y=y+1
+        fact=fact*y
+    return fact==x
+print(factor(120))
