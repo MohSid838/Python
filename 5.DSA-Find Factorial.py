adb529014a15e37fefb4e2fe7539bbfee8a6d71c
Your Name <sid69740@gmail.com>
2025-08-19 12:20:14 +0530

    Inverse Modulo Arithmetic

--- 5.DSA-Find Factorial.py
+++ 5.DSA-Find Factorial.py
@@ -1,18 +1,31 @@
-def fact(x:int):
-    result=1
-    for x in range(1,x+1):
-        result=x*result
-    return result
-print(fact(5))
+# def fact(x:int):
+#     result=1
+#     for x in range(1,x+1):
+#         result=x*result
+#     return result
+# print(fact(5))
 
-def factor(x:int):
-    if x==1 or x==0:
-        return True
-    fact=1
-    y=1
-    while fact<x:
-        y=y+1
-        fact=fact*y
-    return fact==x
-print(factor(120))
+# def factor(x:int):
+#     if x==1 or x==0:
+#         return True
+#     fact=1
+#     y=1
+#     while fact<x:
+#         y=y+1
+#         fact=fact*y
+#     return fact==x
+# print(factor(120))
 #find new
+if __name__ == "__main__":
+    x = input("Enter number 1: ")
+    y = input("Enter number 2: ")
+    try:
+        z = x / int(y)
+
+    except Exception as e:
+        print("Exception type: ", type(e).__name__)
+        print("Exception occurred:", e)
+        z = None
+
+    finally:
+        print(z)
